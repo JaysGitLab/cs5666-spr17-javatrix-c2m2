20513c5aab5547dca8a6a36c15e964507a16c371
Michelle Melton <meltonml@appstate.edu>
2017-02-08 15:21:25 -0500

    Set up initial class file

---
+++ Javatrix.java
@@ -0,0 +1,23 @@
+/**
+ * Javatrix.java
+ *
+ * @author Kara Beason
+ * @author Cydney Caldwell
+ * @author Michelle Melton
+ * @version Feb 2017
+ */
+
+/**
+ * Provides fundamental operations of numerical linear algebra.
+ *
+ * @author Kara Beason
+ * @author Cydney Caldwell
+ * @author Michelle Melton
+ * @version Feb 2017
+ */
+public class Javatrix extends java.lang.Object 
+    implements java.lang.Cloneable, java.io.Serializable 
+{
+    
+    private double[][] matrix;
+}
