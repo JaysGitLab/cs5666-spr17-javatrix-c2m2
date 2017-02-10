fd72b3be9d2594a09b20eefbdb79ecc16751b39d
Kara Beason <KaraBeason@github.com>
2017-02-10 10:48:35 -0500

    Add contructor that takes a 2D array.

--- Javatrix.java
+++ Javatrix.java
@@ -21,4 +21,29 @@ public class Javatrix extends java.lang.Object
     private double[][] matrix;
     int m;
     int n;
+
+    /**
+     ** Construct a matric from a 2D array.
+     **
+     ** @param A - 2D array to be copied into matrix
+     **/
+
+     public Javatrix(double[][] A)
+     {
+         this.m = A[0].length;
+         this.n = A[1].length;
+
+         matrix = new double[m][n];
+
+         for (int i = 0; i < m; i++)
+         {
+             for (int j = 0; j < n; j++)
+             {
+                 matrix[i][j] = A[i][j];
+
+             }
+         }
+     }
+
 }
+
