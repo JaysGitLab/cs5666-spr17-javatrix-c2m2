109ce32bb47951a82c90b0e77d506cb2c1fe4fb8
Kara Beason <KaraBeason@github.com>
2017-02-13 20:16:10 -0500

    Remove accidental extra line.

--- Javatrix.java
+++ Javatrix.java
@@ -192,7 +192,6 @@ public class Javatrix extends java.lang.Object
     public double[][] getArrayCopy()
     {
         double[][] matrixCopy = new double[m][n];
-
         for (int i = 0; i < m; i++)
         {
             for (int j = 0; j < n; j++)
