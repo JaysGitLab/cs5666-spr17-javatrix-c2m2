fd652ad1b770eebb43d2c9fea2d9a042ce239da8
Michelle Melton <meltonml@appstate.edu>
2017-02-16 12:44:47 -0500

    Add unit tests and implement minusEquals, issue #30

--- JavatrixTest.java
+++ JavatrixTest.java
@@ -466,6 +466,42 @@ public class JavatrixTest
     }
 
     /**
+     * Test minusEquals function, valid values.
+     */
+    @Test
+    public void minusEqualsValid()
+    {
+        m = 2;
+        n = 3;
+        double[] a = {1.1, 2.2, 3.3};
+        double[] b = {4.4, 5.5, 6.6};
+        matrix = new double[m][n];
+        matrix[0] = a;
+        matrix[1] = b;
+
+        double[] c = {1.1, 1.2, 1.3};
+        double[] d = {1.4, 1.5, 1.6};
+        double[][] matrixB = new double[m][n];
+        matrixB[0] = c;
+        matrixB[1] = d;
+
+        actual = new Javatrix(matrix);
+        Javatrix actualB = new Javatrix(matrixB);
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i][j] = matrix[i][j] - matrixB[i][j];
+            }
+        }
+
+        actual = actual.minusEquals(actualB);
+        assertArrayEquals("failure - double arrays are not same",
+            matrix, actual.getArray());
+    }
+    
+    /**
      * Tear down after unit tests.
      */
     @After
