62193d1214745098e7fb79af6681ab2fe0be0868
Cydney Caldwell <caldwellc1@appstate.edu>
2017-02-19 17:14:27 -0500

    arrayRDEquals

--- Javatrix.java
+++ Javatrix.java
@@ -477,6 +477,7 @@ public class Javatrix extends java.lang.Object
         }
         return actual;
     }
+
     /**
      * Return calling matrix A = A \ B.
      *
@@ -503,5 +504,30 @@ public class Javatrix extends java.lang.Object
         return this;
     }
 
+    /**
+     * Return calling matrix A = A / B.
+     *
+     * @param actualB calling matrix
+     * @return A / B
+     */
+    public Javatrix arrayRightDivideEquals(Javatrix actualB)
+    {
+        m = actualB.getRowDimension();
+        n = actualB.getColumnDimension();
+        double a;
+        double b;
+        double c;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                a = this.get(i, j);
+                b = actualB.get(i, j);
+                c = a / b;
+                this.set(i, j, c);
+            }
+        }
+        return this;
+    }
 }
  
